ef9f56c8d5b50e0a92d1985c33a9185a196e5f54
Nweke Cynthia Chinenye <nwekecynthia2015@gmail.com>
2025-09-22 10:09:38 -0700

    fixes

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -73,15 +73,15 @@ export default function Navbar() {
       </div>
 
       {/* mobile Menu */}
-      <div className="lg:hidden flex">
+      <div className="lg:hidden flex bg-[#f6f6f6] dark:bg-[#121212]">
         <div
-          className={`fixed top-0 right-0 h-full w-[250px] bg-[#f6f6f6] transform transition-transform duration-500 z-50 ${
+          className={`fixed top-0 right-0 h-full w-[250px]  transform transition-transform duration-500 z-50 ${
             menuOpen ? "translate-x-0" : "translate-x-full"
           } overflow-y-auto`}
         >
           {/* Close button */}
-          <div className="flex justify-between items-center px-2 py-4 text-black">
-            <div className="flex items-center space-x-1 focus:outline-none text-black">
+          <div className="flex justify-between items-center px-2 py-4 text-black dark:text-white">
+            <div className="flex items-center space-x-1 focus:outline-none text-black dark:text-white">
               <div className="flex items-center gap-[5px]">
                 <span>
                   <Radius />
